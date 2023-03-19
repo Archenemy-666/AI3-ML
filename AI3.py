edc46628a963b72a1021d5ea4a93c8ad8a6ff4e1
Archenemy_666 <csiddu98@gmail.com>
2023-03-18 19:15:23 -0500

    created a .py file to work on VaccineSearch

---
+++ AI3.py
@@ -0,0 +1,8 @@
+#read dataset
+#choose the Greedy Search to find the best allocation of 100 Million from the list to reach  most people
+#Use Genetic Algorithm for optimization of the best options that increase the number of people vaccinated within 100 Million
+
+def inputStats():
+    print("number of vaccince companies")
+    ListSize = input()
+    for i in range () 
