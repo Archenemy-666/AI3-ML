9f2cf09646cb020220aa732e926f239e39b8416a
Archenemy_666 <csiddu98@gmail.com>
2023-03-27 11:24:25 -0500

    input design complete

--- AI3.py
+++ AI3.py
@@ -3,6 +3,27 @@
 #Use Genetic Algorithm for optimization of the best options that increase the number of people vaccinated within 100 Million
 
 def inputStats():
-    print("number of vaccince companies")
-    ListSize = input()
-    for i in range () 
+    ListSize = int(input("number of vaccine comp"))
+    List = []    
+    for i in range(ListSize):
+        name = input("enter name of organization: ")
+        cost = int(input("cost quote: "))
+        population = int(input("population it serves "))
+        List.append([name,cost,population]) 
+    return List
+def greedySearch():
+    List = inputStats()
+    from operator import itemgetter
+    List = sorted(List, key=itemgetter(2), reverse = True)
+    return List
+
+
+
+def main():
+    x = greedySearch()
+    print(x)
+
+if __name__ == "__main__":
+    main()
+
+ 
